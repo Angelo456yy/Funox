649ae2d588cf45b725dc0de22f1c96da68c0dc63
Angel Martinez <134995261+Angelo456yy@users.noreply.github.com>
2024-05-24 03:01:49 -0600

    v 1.5

---
+++ sub.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJXHMGpzv2Qgbu5vo_0u5cpgaWnpLCdCM",
+  authDomain: "fun-ox.firebaseapp.com",
+  projectId: "fun-ox",
+  storageBucket: "fun-ox.appspot.com",
+  messagingSenderId: "733801172410",
+  appId: "1:733801172410:web:79387294ff32a2d68ee183"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
